7183b1c864ff36e748f3f144dbe39932bc2b5edc
jhpark <mugenjh@gmail.com>
2020-11-08 13:22:56 +0900

    1290. Convert Binary Number in a Linked List to Integer

--- src/main/java/easy/P1290.java
+++ src/main/java/easy/P1290.java
@@ -8,7 +8,7 @@ import lombok.NoArgsConstructor;
 import static org.assertj.core.api.Assertions.assertThat;
 
 // 1290. Convert Binary Number in a Linked List to Integer
-// https://leetcode.com/problems/convert-binary-number-in-a-linked-list-to-integer/submissions/
+// https://leetcode.com/problems/convert-binary-number-in-a-linked-list-to-integer/
 public class P1290 {
 
     private final StringBuilder sb = new StringBuilder();
